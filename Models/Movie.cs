5d7fac74a58d7e954a69fc24c36cc2837af61ac6
Aigerim Tumbayeva <aigerimt922@gmail.com>
2021-02-13 19:21:54 +0600

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+
+        public int Id { get; set; }
+
+
+        [Required]
+        [StringLength(30)]
+        public string Director { get; set; }
+
+
+        [Required]
+        [StringLength(80)]
+        public string Genre { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Poster { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
